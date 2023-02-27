7ff5cc42d0ef5185ee29e65caac214f9020ae93d
Sajib Hossain <sajibhossain1185@gmail.com>
2023-02-27 11:51:08 +0600

    setup vite eslint and prettier

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+const App = () => {
+  return (
+    <div>
+      <form action="">
+        <input
+          type="text"
+          id="name"
+          className="dinosour"
+          aria-describedby="disnasour name info"
+        />
+      </form>
+    </div>
+  );
+};
+
+export default App;
